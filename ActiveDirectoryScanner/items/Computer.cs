90848326fc711b2dab18618ddafd94d8d047a013
Nesimi <hasanesimi01@gmail.com>
2023-07-13 20:54:33 +0300

    model düzenlendi.

--- ActiveDirectoryScanner/items/Computer.cs
+++ ActiveDirectoryScanner/items/Computer.cs
@@ -4,7 +4,7 @@
     {
         public string objectSid { get; set; }
         public string distinguishedName { get; set; }
-        public string OperatingSystem { get; set; }
+        public string operatingSystem { get; set; }
         public DateTime whenCreated { get; set; }
         public string securityDescriptor { get; set; }
     }
